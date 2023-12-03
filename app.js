4519d6667b3a83b4e2b955104871420da9b6dfba
sushant17-glitch <80514294+sushant17-glitch@users.noreply.github.com>
2023-12-04 01:20:00 +0530

    Added username as googleUser for Oauth

--- app.js
+++ app.js
@@ -38,7 +38,8 @@ const userSchema = new mongoose.Schema ({
   email: String,
   password: String,
   googleId: String,
-  secret: String
+  secret: String,
+  username: String
 });
 
 userSchema.plugin(passportLocalMongoose);
@@ -69,7 +70,7 @@ passport.use(new GoogleStrategy({
   function(accessToken, refreshToken, profile, cb) {
     console.log(profile);
 
-    User.findOrCreate({ googleId: profile.id }, function (err, user) {
+    User.findOrCreate({ googleId: profile.id, username: 'googleUser' }, function (err, user) {
       return cb(err, user);
     });
   }
